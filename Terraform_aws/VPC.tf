327a714c1b4c6577df75190f7652f2ed35c727d1
Big Mag <56141003+bigmagnetic1@users.noreply.github.com>
2023-04-16 02:36:31 -0400

    Update VPC.tf

--- Terraform_aws/VPC.tf
+++ Terraform_aws/VPC.tf
@@ -2,8 +2,8 @@
 
 
   resource "aws_vpc" "main" {
-  cidr_block       = "172.31.0.0/16"
-  instance_tenancy = "default"
+  cidr_block       = "10.0.0.0/16"
+  #instance_tenancy = "default"
 
   tags = {
     Name = "main"
